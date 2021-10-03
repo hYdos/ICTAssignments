77f269ee800f7fc0f4e639da528861280860e427
Ramid Khan <ramidzkh@gmail.com>
2021-10-03 15:57:31 +1100

    More idiomatic Rust

--- WebAssembly/src/utils.rs
+++ WebAssembly/src/utils.rs
@@ -1,26 +1,27 @@
-use js_sys::{JsString, Object};
+use js_sys::JsString;
 use wasm_bindgen::JsCast;
 use wasm_bindgen::prelude::*;
-use web_sys::{Document, HtmlCanvasElement, WebGlProgram, WebGlRenderingContext, WebGlShader};
+use web_sys::{Document, HtmlCanvasElement, WebGlRenderingContext};
 
 pub fn set_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
-pub fn get_document() -> Document {
-    return web_sys::window().unwrap().document().unwrap();
+pub fn get_document() -> Result<Document, JsValue> {
+    Ok(web_sys::window().ok_or(JsString::from("Window not present"))?
+        .document().ok_or(JsString::from("Document not present"))?)
 }
 
 pub fn get_gl(canvas: &str) -> Result<WebGlRenderingContext, JsValue> {
-    return Ok(get_canvas(canvas)?
-        .get_context("webgl")?.ok_or(JsValue::null())?
-        .dyn_into::<WebGlRenderingContext>()?);
+    Ok(get_canvas(canvas)?
+        .get_context("webgl")?.ok_or(JsString::from("WebGL context not found"))?
+        .dyn_into::<WebGlRenderingContext>()?)
 }
 
 pub fn get_canvas(canvas: &str) -> Result<HtmlCanvasElement, JsValue> {
-    return Ok(get_document()
+    Ok(get_document()?
         .get_element_by_id(canvas).ok_or(JsString::from("Canvas element not found"))?
-        .dyn_into::<web_sys::HtmlCanvasElement>()?);
+        .dyn_into::<web_sys::HtmlCanvasElement>()?)
 }
 
 pub trait JsStringExtensions {
